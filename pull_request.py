dd3a19de473a1bd02239fd5940fc17d670ba7845
Flora Park <flora.park@wpa3353.corp.warbyparker.com>
2016-06-16 18:10:09 -0400

    fixed file for pylint and UTC

--- pull_request.py
+++ pull_request.py
@@ -18,7 +18,7 @@ all_power = lifx.get_power_all_lights()
 all_color = lifx.get_color_all_lights()
 
 # ---------------------------------------------------------------------------- #
-# helios
+
 helios = "https://api.github.com/repos/WarbyParker/helios/pulls?sort=updated&direction=dsc"
 response = requests.get(helios, auth=('floramjp', 'd1f69bc35998d2066908f443f91192e67fb91dcd'))
 response_json = response.json()
@@ -29,30 +29,29 @@ load_dict = json.loads(response_dict)
 print "Initialization! Current time is: " + datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
 init_time = datetime.datetime.now() - datetime.timedelta(minutes=20)
 
-while True :
+while True:
 	for x in load_dict:
 		tics_time = datetime.datetime.strptime(x["updated_at"], "%Y-%m-%dT%H:%M:%SZ")
 		if tics_time > init_time:
-			# Note: Github API brings thins in +4 hours time, so will neutralize
-			tics_str = (tics_time - datetime.timedelta(hours=4)).strftime("%Y-%m-%dT%H:%M:%SZ")
-			print "************************************************************"
-			print "PR number: " + str(x["number"])
-			print "PR user login: " + x["user"]["login"]
-			print "PR title: " + x["title"]
-			print "PR updated_at timestamp: " + tics_str
-			x["updated_at"]
-			print "************************************************************"
-			init_time = tics_time
-		
-			# alert bulb
-			bulb.set_color(BLUE)
-			sleep(1)
-			bulb.set_color(original_color)
+		    # Time: UTC
+		    tics_str = tics_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+		    print "************************************************************"
+		    print "PR number: " + str(x["number"])
+		    print "PR user login: " + x["user"]["login"]
+		    print "PR title: " + x["title"]
+		    print "PR updated_at timestamp: " + tics_str
+		    print "************************************************************"
+		    init_time = tics_time
+
+		    # alert bulb
+		    bulb.set_color(BLUE)
+		    sleep(1)
+		    bulb.set_color(original_color)
 	
-	# sleep for 5 minutes
-	sleep(300)
-	response = requests.get(helios, auth=('floramjp', 'd1f69bc35998d2066908f443f91192e67fb91dcd'))
-	response_json = response.json()
-	response_dict = json.dumps(response_json)
-	load_dict = json.loads(response_dict)
-	print "Current time is: " + datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    # sleep for 5 minutes
+    sleep(300)
+    response = requests.get(helios, auth=('floramjp', 'd1f69bc35998d2066908f443f91192e67fb91dcd'))
+    response_json = response.json()
+    response_dict = json.dumps(response_json)
+    load_dict = json.loads(response_dict)
+    print "Current time is: " + datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
